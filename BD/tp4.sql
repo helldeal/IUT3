4f466c0a97120e1bf3944dde41a2c3e556ba8c8e
helldeal <alexandreclenet@free.fr>
2023-10-06 15:39:36 +0200

    BD

--- BD/tp4.sql
+++ BD/tp4.sql
@@ -114,3 +114,173 @@ db.vendeur.aggregate([
   { $match: { 'produitInfo.libelle': 'chou' } },
   { $project: { 'nom': 1, '_id': 0 } }
 ]);
+
+
+db.facture.insertMany([
+  { _idf: 1, idc: "Dupond", date: new Date("2023-02-01"), montant: 95, ventes: [{ idp: 1,
+qte: 5 }, { idp: 4, qte: 2 }, { idp: 3, qte: 3 }], etat: "A" },
+{ _idf: 2, idc: "Dupond", date: new Date("2023-03-05"), montant: 62, ventes: [{ idp:
+2, qte: 4 }, { idp: 8, qte: 3 }], etat: "O" },
+{ _idf: 3, idc: "Dupond", date: new Date("2023-07-10"), montant: 100, ventes: [{ idp:
+7, qte: 2 }, { idp: 2, qte: 6 }, { idp: 1, qte: 5 }], etat: "O" },
+{ _idf: 4, idc: "Lucas", date: new Date("2023-08-17"), montant: 52, ventes: [{ idp:
+7, qte: 7 }], etat: "A" },
+{ _idf: 5, idc: "Lucas", date: new Date("2023-03-20"), montant: 75, ventes: [{ idp:
+1, qte: 1 }, { idp: 2, qte: 7 }], etat: "A" },
+{ _idf: 6, idc: "Sophie", date: new Date("2023-03-21"), montant: 40, ventes: [{ idp:
+3, qte: 2 }], etat: "O" },
+{ _idf: 7, idc: "Eliot", date: new Date("2023-11-05"), montant: 45, ventes: [{ idp:
+4, qte: 3 }, { idp: 2, qte: 3 }], etat: "A" },
+{ _idf: 8, idc: "Eliot", date: new Date("2023-12-20"), montant: 25, ventes: [{ idp:
+2, qte: 2 }, { idp: 1, qte: 1 }], etat: "O" },
+{ _idf: 9, idc: "Martin", date: new Date("2023-05-10"), montant: 20, ventes: [{ idp:
+5, qte: 4 }], etat: "O" },
+{ _idf: 10, idc: "Martin", date: new Date("2023-11-04"), montant: 32, ventes: [{ idp:
+4, qte: 2 }, { idp: 8, qte: 3 }], etat: "A" }
+]);
+
+
+var mapFunction = function () {
+  emit(this.idc, this.montant);
+};
+
+var reduceFunction = function (key, values) {
+  return Array.sum(values);
+};
+
+db.facture.mapReduce(
+  mapFunction,
+  reduceFunction,
+  { out: { inline: 1 } }
+);
+
+
+db.facture.mapReduce(
+  mapFunction,
+  reduceFunction,
+  { out: "Temp1" }
+);
+
+db.Temp1.find().sort({ '_id': 1 });
+
+db.facture.aggregate([
+  { $group: { '_id': '$idc', 'totalMontant': { $sum: '$montant' } } }
+]);
+
+var mapFunction6 = function () {
+  emit(this.idc, 1);
+};
+
+var reduceFunction6 = function (key, values) {
+  return Array.sum(values);
+};
+
+db.facture.mapReduce(
+  mapFunction6,
+  reduceFunction6,
+  { out: { inline: 1 } }
+);
+
+db.facture.aggregate([
+  { $group: { '_id': '$idc', 'count': { $sum: 1 } } }
+]);
+
+var mapFunction8 = function () {
+  this.ventes.forEach(function (vente) {
+    emit(vente.idp, 1);
+  });
+};
+
+var reduceFunction8 = function (key, values) {
+  return Array.sum(values);
+};
+
+db.facture.mapReduce(
+  mapFunction8,
+  reduceFunction8,
+  { out: { inline: 1 } }
+);
+
+var mapFunction9 = function () {
+  var client = this.idc;
+  this.ventes.forEach(function (vente) {
+    emit({ idp: vente.idp, client: client }, 1);
+  });
+};
+
+var reduceFunction9 = function (key, values) {
+  return Array.sum(values);
+};
+
+db.facture.mapReduce(
+  mapFunction9,
+  reduceFunction9,
+  { out: { inline: 1 }, query: { 'idc': 'Eliot' } }
+);
+
+db.facture.aggregate([
+  { $match: { 'idc': 'Eliot' } },
+  { $unwind: '$ventes' },
+  { $group: { '_id': { idp: '$ventes.idp', client: '$idc' }, 'count': { $sum: 1 } } }
+]);
+
+var mapFunction11 = function () {
+  this.ventes.forEach(function (vente) {
+    emit(vente.idp, vente.qte);
+  });
+};
+
+var reduceFunction11 = function (key, values) {
+  return Array.sum(values);
+};
+
+db.facture.mapReduce(
+  mapFunction11,
+  reduceFunction11,
+  { out: { inline: 1 } }
+);
+
+
+var mapFunction12 = function () {
+  var dateLimite = new ISODate('2023-03-10T00:00:00Z');
+  this.ventes.forEach(function (vente) {
+    if (this.date <= dateLimite) {
+      emit(vente.idp, vente.qte);
+    }
+  });
+};
+
+db.facture.mapReduce(
+  mapFunction12,
+  reduceFunction11,
+  { out: { inline: 1 } }
+);
+
+
+var mapFunction13 = function () {
+  var dateLimite = new ISODate('2023-03-10T00:00:00Z');
+  this.ventes.forEach(function (vente) {
+    if (this.date <= dateLimite) {
+      emit(vente.idp, { total: vente.qte, count: 1 });
+    }
+  });
+};
+
+var reduceFunction13 = function (key, values) {
+  var reduced = { total: 0, count: 0 };
+  values.forEach(function (value) {
+    reduced.total += value.total;
+    reduced.count += value.count;
+  });
+  return reduced;
+};
+
+var finalizeFunction13 = function (key, reduced) {
+  return reduced.total / reduced.count;
+};
+
+db.facture.mapReduce(
+  mapFunction13,
+  reduceFunction13,
+  { out: { inline: 1 }, finalize: finalizeFunction13 }
+);
